5da2274d4798a6033cdcbdf53781d4d619a8fb5d
AlexzzZ-0 <qz2322@nyu.edu>
2023-04-28 17:06:33 -0400

    have the sence of delay

--- pace_prototype/Assets/Scripts/RodManager.cs
+++ pace_prototype/Assets/Scripts/RodManager.cs
@@ -16,7 +16,12 @@ public class RodManager : MonoBehaviour
 
     [SerializeField] private GameObject _fishHolder;
     [SerializeField] private Image _fish;
+
+    [SerializeField] private GameObject _seaHolder;
+    
     public float endValue = 100f;
+    private float endX = 477f;
+    private float endY = 310f;
 
     public float _rodTime = 2f;
     public float _lineTime = 2f;
@@ -62,7 +67,9 @@ public class RodManager : MonoBehaviour
         //set everything to initial settings
         _lineHolder.transform.DOScale(new Vector3(0, 0, 0), .1f);
         _changeTime = 0;
-        _fishHolder.transform.DOMoveY(90, .1f);
+        _fishHolder.transform.DOMove(new Vector3(477,70), .1f);
+        
+        JitterSea();
     }
 
     
@@ -81,8 +88,23 @@ public class RodManager : MonoBehaviour
 
                 RotateRod();
                 Invoke("ExpandLine", _rodTime + _changeTime);
-                Invoke("CatchFish", _rodTime + _lineTime - _changeTime);
+                Invoke("CatchFish", _rodTime + _lineTime - _changeTime * 2f);
+                Invoke("JitterSea",_rodTime + _changeTime + _lineTime * .85f);//sea jitters when the line casts in
 
+                //make a glitch here
+                if ((_lineTime - _changeTime * .5f) >= 0)
+                {
+                    _lineTime = 2f - _changeTime * .5f;
+                    _fishTime = 2f - _changeTime * .5f;
+                }
+                else
+                {
+                    _lineTime = .1f;
+                    _fishTime = .1f;
+                }
+                
+                
+                
                 count += 1;
             }
         }
@@ -110,14 +132,42 @@ public class RodManager : MonoBehaviour
 
     void ExpandLine()
     {
-        _lineHolder.transform.DOScale(new Vector3(1, 1, 1), _lineTime);
+        _lineHolder.transform.DOScale(new Vector3(1, 1, 1), _lineTime).SetEase(Ease.InBounce);
         _lineexpandDone = true;
     }
 
     void CatchFish()
     {
-        _fishHolder.transform.DOMoveY(endValue, _fishTime);
+        endValue += count * 5;
+
+        if (endValue >= 310f)
+        {
+            endValue = 310f;
+            _fishTime *= .5f;
+
+            endX += (count - 7) * 5;
+            endY -= (count - 7) * 4;
+            
+            _fishHolder.transform.DOMoveY(endValue, _fishTime).OnComplete(() =>
+            {
+                _fishHolder.transform.DORotate(new Vector3(0, 0, -120), _fishTime).SetEase(Ease.InBounce).OnComplete(
+                    () =>
+                    {
+                        _fishHolder.transform.DOMove(new Vector3(endX, endY), _fishTime);
+                    });
+            });
+        }
+        else
+        {
+            _fishHolder.transform.DOMoveY(endValue, _fishTime);
+        }
+        
         _catchfishDone = true;
+    }
+
+    void JitterSea()
+    {
+        _seaHolder.transform.DOJump(new Vector3(430, 250, 0), count, 1,1f);
     }
 
     void Restart()
@@ -130,11 +180,12 @@ public class RodManager : MonoBehaviour
         //the animation of ting everything to the initial settings
         _fish.DOFade(0, .1f).OnComplete(() =>
         {
+            _fishHolder.transform.DORotate(Vector3.zero, .5f);
             _lineHolder.transform.DOScale(new Vector3(0, 0, 0), .1f).OnComplete(() =>
             {
                 _rodHolder.transform.DORotate(new Vector3(0, 0, 0), .1f).OnComplete(() =>
                 {
-                    _fishHolder.transform.DOMoveY(90, .5f).OnComplete(() =>
+                    _fishHolder.transform.DOMove(new Vector3(477,70), .5f).OnComplete(() =>
                     {
                         _fish.DOFade(1, 1f).OnComplete(() =>
                         {
